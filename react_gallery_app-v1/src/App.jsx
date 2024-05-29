ec799bebdeae5ef162d392a4104c727ec566e236
HerrWal <85037202+HerrWal@users.noreply.github.com>
2024-05-28 20:43:59 -0400

    Vite and Flickr
    Adding Vite configuration and Flicker API

--- react_gallery_app-v1/src/App.jsx
+++ react_gallery_app-v1/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import apiKey from './config';
+
+const apiKey = apiKey;
 
 function App() {
   const [count, setCount] = useState(0)
