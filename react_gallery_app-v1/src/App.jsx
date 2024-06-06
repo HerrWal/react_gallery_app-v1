2e17e08559e5226d635b7f3f772ccdbbfce05f14
Juan Agustin Herrera Walwyn <85037202+HerrWal@users.noreply.github.com>
2024-06-06 14:05:15 -0400

    Update App.jsx

--- react_gallery_app-v1/src/App.jsx
+++ react_gallery_app-v1/src/App.jsx
@@ -13,23 +13,10 @@ const apiKey = apiKey;
 
 function App() {
   const fetchData = query => {
-    const jsonData = fetch('https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=a5e446d6ec176bbb74185750d3863836&tags=sunsets&per_page=24&format=json&nojsoncallback=1');
+    const jsonData = fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`);
   };
   return (
     <div className='container'>
-    /**
-    /Home
-    /cats
-    /dogs
-    /computers
-    /search:query
-
-    - Use NavLink components in your Nav component for the 
-    3 default topics.
-
-    - The current route should always be reflected in the URL.
-
-    */
       <Routes>
         <Route path='/' element={<Navigate replace to="/cats" />}></Route>
         <Route path='/cats' element={<PhotoList />}></Route>
