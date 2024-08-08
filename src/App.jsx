3093464a4c7fce0375073bb095eaff8f3b7fe3fe
HerrWal <85037202+HerrWal@users.noreply.github.com>
2024-08-07 21:36:57 -0400

    Aug 7 2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useEffect, useState } from 'react'
 import apiKey from './config'
 import { Route, Routes, Navigate } from 'react-router';
 
@@ -14,22 +13,27 @@ let userApiKey;
 
 function App() {
 
-  const fetchData = query => {
-    return fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${myApiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`);
-  };
-  
-  console.log(fetchData(cats));
+  const [photos, setPhoto] = useState({});
 
-  const [data, setData] = useState(fetchData);
+  const summitHandler = () => {
+    
+  }
+
+  useEffect(() => {
+    fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${myApiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
+      .then(response => response.json())
+      .then(responseData => setPhoto(responseData))
+      .catch(error => console.log("Error fetching and parsing data", error));
+  }, []);
 
   return (
     <div className='container'>
       <Routes>
         <Route path='/' element={<Navigate replace to="/cats" />}></Route>
-        <Route path='/cats' element={<PhotoList photos={data} title={'title'} />}></Route>
-        <Route path='/dogs' element={<PhotoList photos={data} title={'title'} />}></Route>
-        <Route path='/computers' element={<PhotoList photos={data} title={'title'} />}></Route>
-        <Route path='/search/:query' element={<PhotoList photos={data} title={'title'} />}></Route>
+        <Route path='/cats' element={<PhotoList photos={photos} title={'Cats Photos'} />}></Route>
+        <Route path='/dogs' element={<PhotoList photos={photos} title={'Dogs Photos'} />}></Route>
+        <Route path='/computers' element={<PhotoList photos={photos} title={'Computers Photos'} />}></Route>
+        <Route path='/search/:query' element={<PhotoList photos={photos} title={query} />}></Route>
       </Routes>      
       <Photo />
       <Nav />
